3d240778d2a13f9dbb3b1848410e6c11cd17044a
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-09-26 21:17:14 -0500

    update db/seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,7 @@
 populate the burgers table with at least three entries here
+INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whataburger', 'false');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Butterburger', 'false');
+
