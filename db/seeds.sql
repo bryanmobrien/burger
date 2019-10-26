889a17b28ece5c0dfa93c031c77f6b4bbe7c2ac5
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-26 17:14:30 -0500

    initial commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,5 @@
-populate the burgers table with at least three entries here
 INSERT INTO burgers (burger_name, devoured) VALUES ('BigMac', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Double-Double', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Whataburger', 0);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Butterburger', 0);
-
