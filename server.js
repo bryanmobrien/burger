60c3853b90cd4d2d4ec208f8a4ccd00a29ce2916
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-26 18:04:47 -0500

    heroku deploy continue - modify port for favicon  and / H10 errors

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.set("view engine", "handlebars");
 const routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
-app.listen(PORT, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App now listening at localhost:" + PORT);
 });
