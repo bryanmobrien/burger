0db4318f1712b837b86d4ebd187e1dae055cc937
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-26 19:21:09 -0500

    heroku er_no_such_table

--- config/connection.js
+++ config/connection.js
@@ -20,6 +20,13 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
+
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+  
+    console.log('The solution is: ', rows[0].solution);
+  });
+
   console.log("connected as id " + connection.threadId);
 });
 
