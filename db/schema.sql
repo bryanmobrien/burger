672870b2ea45717885ecab3c12f0d4c4ad25fe80
Bryan O'Brien <bryan.m.obrien@gmail.com>
2019-10-26 20:39:54 -0500

    arrrrggghh

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 CREATE TABLE burgers (
-  id INT AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(255),
-  devoured BOOLEAN not null default 0, 
+  id INT NOT NULL AUTO_INCREMENT,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN default 0, 
   PRIMARY KEY (id)
 );
